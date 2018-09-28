7157914afb2c4e8dce3b906d768add2c823fcf2d
Big-Tree <william.murphy333@gmail.com>
2018-09-28 17:35:16 +0100

    Added get_contrilateral
    When select_and_copy_dicom_images is run it now copies the contrilateral
    images to a folder as well as the images with ROIs.

--- cropDicom.py
+++ cropDicom.py
@@ -1,28 +1,98 @@
 from cropDicomFunctions import *
 # _______Conterlateral____________
-# Get file names
-def save_contrilateral_images():
+# Get file names of selected lesion dicom images
+# Using the spreadsheet find the contrilateral images
+# Copy these images to a folder
+
+def get_contrilateral(file_list_lesion, dst_copy_cont, all_dicom_files, spreadsheet, batch = 1):
     import pandas as pd
-    lesionPath = '/vol/research/mammo2/will/data/batches/roi/batch_1/lesions'
-    fileListNames = getFileNames(lesionPath)
+    from shutil import copyfile
+    import pydicom
+    import fnmatch
+    xls = pd.ExcelFile(spreadsheet)
+    sheet1 = xls.parse(1)
+    sheet0 = xls.parse(0)
 
-    xls = pd.ExcelFile(SPREADSHEET)
-    sheet = xls.parse(0)
-    contralaterals = []
-    for _ in fileListNames:
-        imageSOPIUD = os.path.basename(_)[:-4]
-        tmp = getContralateral(imageSOPIUD, sheet)
-        if tmp != None:
-            contralaterals.append(tmp)
-    print(len(contralaterals))
-    print(len(contralaterals[5]))
-    print(contralaterals[1])
+    print('Finding the contrilateral images...')
+    cont_image_paths_to_copy = []
+    for f_index, f in enumerate(file_list_lesion):
+        focus_ImageSOPIUID = os.path.basename(f)[:-4]
+        # Get lesions properties
+        properties = {'StudyIUID': '', 'ImageLaterality': '', 'ViewPosition':
+                      '', 'PresentationIntentType': ''}
+        focus_properties = dict(properties)
+        for _ in focus_properties:
+            focus_properties[_] = getSpreadsheetCell(_, focus_ImageSOPIUID,
+                                                     sheet0)
+        # Calculate properties to search for for contilateral
+        properties_to_match = dict(focus_properties)
+        if properties_to_match['ImageLaterality'] == 'R':
+            properties_to_match['ImageLaterality'] = 'L'
+        else:
+            properties_to_match['ImageLaterality'] = 'R'
 
-# ___________Get ROIs______________
+        # Search spreadsheet for contrilateral images
+        tmp_properties = dict(properties)
+        matches = {'ImageSOPIUID':[], 'path':[]}
+        for tmp_ImageSOPIUID in sheet0['ImageSOPIUID']:
+            for _ in tmp_properties:
+                tmp_properties[_] = getSpreadsheetCell(_, tmp_ImageSOPIUID,
+                                                       sheet0)
+            if tmp_properties == properties_to_match:
+                matches['ImageSOPIUID'].append(tmp_ImageSOPIUID)
+        # Get file path of the matches
+        for match_ImageSOPIUID in matches['ImageSOPIUID']:
+            search = fnmatch.filter(
+                all_dicom_files, '*' + match_ImageSOPIUID + '*')
+            matches['path'].append(search[0])
+
+        print(len(matches['ImageSOPIUID']), ' matches', '(', f_index, '/',
+              len(file_list_lesion), ')', 'batch ', batch)
+        if len(matches['path']) != 0:
+            # Sometimes there are no contrilateral images, sometimes there is
+            # more than 1
+            cont_image_paths_to_copy.append(matches['path'][0])
+
+    # Copy the contilateral images to a folder
+    print('Copying the contrilateral patches...')
+    for index, path in enumerate(cont_image_paths_to_copy):
+        copyfile(path, dst_copy_cont + '/' +
+                 os.path.basename(cont_image_paths_to_copy[index]))
+        #print(os.path.basename(
+        #    cont_image_paths_to_copy[index]), 
+        #    '\nis cont to:\n',
+        #    os.path.basename(file_list_lesion[index]),
+        #    '\n')
+        print(index + 1, '/', len(cont_image_paths_to_copy), 'batch ', batch )
+
+
+
+def contrilateral_patches():
+    for f in file_list_lesion:
+        studyIUID = os.path.basename(f)[:-4]
+        indx = [_ == key for _ in sheet['ImageSOPIUID']].index(True)
+        # Get ROI coords
+        roi = {'x1': '', 'x2': '', 'y1': '' ,'y2': '', 'image_width': ''}
+        roi['x1'] = getSpreadsheetCell('X1', imageSOPIUID, sheet1)
+        roi['x2'] = getSpreadsheetCell('X2', imageSOPIUID, sheet1)
+        roi['y1'] = getSpreadsheetCell('Y1', imageSOPIUID, sheet1)
+        roi['y2'] = getSpreadsheetCell('Y2', imageSOPIUID, sheet1)
+        # Get image width
+        img = pydicom.dcmread(f)
+        roi['image_width'] = img.Columns
+
+        # Compute contrilateral coords
+        roi_cont = {'x1': '', 'x2': '', 'y1': '' ,'y2': ''}
+        roi_cont['x1'] = roi['image_width'] - roi['x2']
+        roi_cont['x2'] = roi['image_width'] - roi['x1']
+        roi_cont['y1'] = roi['y1']
+        roi_cont['y2'] = roi['y2']
+
+
 
 # Get list of files that are for presentation and have ROIs
 # Copy these files to a new folder
-def get_ROIs(dstCopy, lesion_path, spreadsheet, batch = 1):
+def get_ROIs(dst_copy_lesion, lesion_path, spreadsheet, batch = 1, copy = True):
     import pandas as pd
     from shutil import copyfile
     xls = pd.ExcelFile(spreadsheet)
@@ -37,9 +107,11 @@ def get_ROIs(dstCopy, lesion_path, spreadsheet, batch = 1):
     # Remove files so that there is only one per studyIUID
     file_list = filter_for_single_StudyIUID(file_list, sheet1)
 
-    for index, path in enumerate(file_list):
-        copyfile(path, dstCopy + '/' + os.path.basename(file_list[index]))
-        print(index, '\\', len(file_list), 'batch ', batch )
+    if copy == True:
+        for index, path in enumerate(file_list):
+            copyfile(path, dst_copy_lesion + '/' + os.path.basename(file_list[index]))
+            print(index, '\\', len(file_list), 'batch ', batch )
+    return file_list
 
 
 # This will copy images to a separate folder that have both ROIs, are for
@@ -55,9 +127,16 @@ def select_and_copy_dicom_images():
         spreadsheet = ('/vol/research/mammo2/will/data/batches/metadata/' +
             str(batch) + '/batch_' + str(batch) + '_IMAGE.xls')
 
-        get_ROIs('/vol/research/mammo2/will/data/batches/roi/batch_' +
+        lesion_file_list = get_ROIs('/vol/research/mammo2/will/data/batches/roi/batch_' +
             str(batch) + '/lesions_for_presentation_one_per_studyIUID',
-            dicom_files, spreadsheet)
+            dicom_files, spreadsheet, copy = False)
+
+        dst_copy_cont = ('/vol/research/mammo2/will/data/batches/roi/batch_' +
+                         str(batch) +
+                         '/cont_for_presentation_one_per_studyIUID')
+        all_dicom_files = getFileNames(dicom_files)
+        get_contrilateral(lesion_file_list, dst_copy_cont, all_dicom_files,
+                          spreadsheet, batch)
 
 
 # Create patches from the images that we separated out
@@ -107,8 +186,8 @@ def main():
     '/vol/research/mammo2/will/data/batches/roi/batch_1/lesions_for_presentation_one_per_studyIUID/')
     patch_write_location = '/vol/research/mammo2/will/data/batches/roi/'
 
-    create_patches(CROP_SIZE, patch_write_location, SPREADSHEET)
-    #select_and_copy_dicom_images()
+    #create_patches(CROP_SIZE, patch_write_location, SPREADSHEET)
+    select_and_copy_dicom_images()
 
 if __name__ == "__main__":
     main()
